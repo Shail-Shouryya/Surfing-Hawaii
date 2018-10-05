e2e76593a602efbfebce2f5c54f533152ce97be0
Shail Shouryya <sshouryy@ucsc.edu>
2018-10-04 23:32:22 -0700

    added comments to api, shortened .py file so easier to call

--- call.py
+++ call.py
@@ -8,14 +8,27 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 # Database Set up
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect = True)
 
 #save tables as variables
-#Measurement = Base.classes.measurement
+### not sure why getting ###
+#KeyError: 'measurement'
+#
+#During handling of the above exception, another exception occurred:
+#
+#Traceback (most recent call last):
+#  File "call.py", line 16, in <module>
+#    Measurement = Base.classes.measurement
+#  File "C:\Users\Shail\Anaconda3\lib\site-packages\sqlalchemy\util\_collections.py", line #212, in __getattr__
+#    raise AttributeError(key)
+#AttributeError: measurement
+Measurement = Base.classes.measurement
 Station = Base.classes.station
 session = Session(engine)
+
+### fixed KeyError, was forgetting to refernce SQLite file from Resources directory ###
 
 ##### Set up Flask
 app = Flask(__name__)
